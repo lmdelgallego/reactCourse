4e39ae830fe7f756b3806d91abb7ca2d3617b9a5
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-27 09:09:31 -0500

    ✨feature: S6-106 - 107: Simular un submit del formulario - Jest Mock Functions

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -11,4 +11,26 @@ describe('AddCategory component', () => {
     // screen.debug();
 
   });
+  test('should not be submit', () => {
+    const onNewCategory = jest.fn()
+    render(<AddCategory onNewCategory={onNewCategory} />)
+    const input = screen.getByRole('textbox')
+    const form = screen.getByRole('form')
+    fireEvent.input(input, { target: { value: '' } })
+    fireEvent.submit(form)
+    expect(onNewCategory).not.toHaveBeenCalled()
+  });
+  test('should be submit', () => {
+    const onNewCategory = jest.fn()
+    const inputValue = 'Saitama';
+    render(<AddCategory onNewCategory={onNewCategory} />)
+    const input = screen.getByRole('textbox')
+    const form = screen.getByRole('form')
+    fireEvent.input(input, { target: { value: inputValue } })
+    fireEvent.submit(form)
+    expect(onNewCategory).toHaveBeenCalledTimes(1)
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue)
+    expect(input.value).toBe('');
+  });
+
 });
