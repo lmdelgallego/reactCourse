c4108159748504a1f9e54be5795a67ff67ef11ac
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-26 20:59:33 -0500

    ✨feature: S6-103: Pruebas GifItem

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,13 +1,23 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import {GifItem} from '../../src/components/GifItem';
 describe('GifItem', () => {
 
   const url = 'https://localhost/algo.jpg';
-  const title = 'Un titulo';
+  const title = 'Saitama';
 
   test('should be match with snapshot', () => {
-
     const { container } = render(<GifItem title={ title } url={url} />)
     expect(container).toMatchSnapshot();
+  });
+
+  test('should be have a paragraph with title', () => {
+    render(<GifItem title={title} url={url} />)
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+
+  test('should be have a image with url and alt', () => {
+    render(<GifItem title={title} url={url} />)
+    expect(screen.getByRole('img').src).toBe(url);
+    expect(screen.getByRole('img').alt).toBe(title);
   });
 });
