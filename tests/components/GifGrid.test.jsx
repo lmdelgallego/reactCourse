cba6ad7eb4ed5fb55d07de3f20f16bea36561eb1
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-27 19:13:01 -0500

    ✨feature: S6-108: Hacer un mock completo de un Custom Hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,16 +1,37 @@
 import {render, screen } from '@testing-library/react';
 import GifGrid from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 describe('GifGrid', () => {
 
   const category = 'One Punch';
   test('should be show loading', () => {
+    useFetchGifs.mockReturnValue({
+      gifs: [],
+      loading: true
+    });
     render(<GifGrid category={category} />)
-    screen.debug();
     expect(screen.getByText('Cargando...'))
     expect(screen.getByText(category))
   });
 
   test('should be show items when images are loaded useFetchGifs', () => {
-
+    const gifs = [{
+      id: 'ABC',
+      url: 'https://localhost/cualquier/cosa.jpg',
+      title: 'Cualquier cosa'
+    },
+    {
+      id: '123',
+      url: 'https://localhost/cualquier/goku.jpg',
+      title: 'GOKU'
+    },];
+    useFetchGifs.mockReturnValue({
+      gifs,
+      loading: false
+    });
+    render(<GifGrid category={category} />)
+    expect(screen.getAllByRole('img').length).toBe(gifs.length)
   });
 });
